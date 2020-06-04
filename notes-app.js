84dadd83742e8ba8c19a8ff13ec1d4b102680617
joseliner <joselineruiz917@gmail.com>
2020-06-03 22:01:26 -0400

    OM

---
+++ notes-app.js
@@ -0,0 +1,11 @@
+// DOM : Document Object Model
+
+// const p = document.querySelector('p')
+// p.remove()
+
+
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function(p){
+    p.textContent = 'bbewoihdnc'
+})
